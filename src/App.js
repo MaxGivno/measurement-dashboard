94419cd4f539e291b138fbd7bc8aa0e48384b663
Max Givno <max.ryazanov@gmail.com>
2020-11-19 23:10:39 +0200

    Comments added

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@ import './App.css'
 import { useEffect, useState } from 'react'
 import Block from './components/Block'
 
+// Creating socket connection
 const socket = new WebSocket('wss://jsdemo.envdev.io/ws')
 
+// Monitoring socket
 socket.onopen = (e) => {
   console.log('Connection established')
 }
 
+// Custom sort fuction
 function compare(a, b) {
   if (a._id < b._id) {
     return -1
@@ -19,37 +22,47 @@ function compare(a, b) {
 }
 
 function App() {
+  // State setup
   const [data, setData] = useState([])
 
   useEffect(() => {
+    // Getting data from server
     socket.onmessage = (e) => {
       let newData = JSON.parse(e.data)
 
+      // Update state with received data
       setData((prevData) => {
+        // If state is empty, fill it with received data
         if (prevData === []) {
           return newData.sort(compare)
         }
 
+        // If state is not empty, create a copy
         let updatedData = [...prevData]
 
+        // Iterate through new data
         newData.forEach((obj) => {
+          // Find if state has object with same id
           let index = updatedData.findIndex(
             (prevObj) => prevObj._id === obj._id
           )
           if (index === -1) {
+            // If it hasn't, add a new object
             updatedData.push(obj)
           } else {
+            // If it has, update current object with new data
             if (obj.measurements.length > 0) {
               updatedData[index].measurements = obj.measurements
             }
           }
         })
-
+        // Update state
         return updatedData.sort(compare)
       })
     }
 
     return () => {
+      // Close the socket on component unmount
       socket.close()
       socket.onclose = (e) => {
         if (e.wasClean) {
