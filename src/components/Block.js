737ddd0b53e7f01f08385931e02965f103349609
Max Givno <max.ryazanov@gmail.com>
2020-11-20 10:07:50 +0200

    Displaying values for different types of data

--- src/components/Block.js
+++ src/components/Block.js
@@ -1,25 +1,56 @@
 import React from 'react'
 
+function toDegreesMinutesAndSeconds(coordinate) {
+  var absolute = Math.abs(coordinate)
+  var degrees = Math.floor(absolute)
+  var minutesNotTruncated = (absolute - degrees) * 60
+  var minutes = Math.floor(minutesNotTruncated)
+  var seconds = Math.floor((minutesNotTruncated - minutes) * 60)
+
+  return `${degrees}° ${minutes}' ${seconds}"`
+}
+
+function convertDMS(lat, lng) {
+  var latitude = toDegreesMinutesAndSeconds(lat)
+  var latitudeCardinal = lat >= 0 ? 'N' : 'S'
+
+  var longitude = toDegreesMinutesAndSeconds(lng)
+  var longitudeCardinal = lng >= 0 ? 'E' : 'W'
+
+  return (
+    <>
+      {latitude} <span>{latitudeCardinal}</span>
+      <br />
+      {longitude} <span>{longitudeCardinal}</span>
+    </>
+  )
+}
+
 const Block = ({ name, unit, measurements }) => {
+  const value =
+    measurements.length > 0 && measurements[measurements.length - 1][1]
   let recentValue =
-    measurements.length > 0 &&
-    typeof measurements[measurements.length - 1][1] === 'number' &&
-    measurements[measurements.length - 1][1]
+    typeof value === 'number'
+      ? Number(value).toFixed(2)
+      : typeof value === 'string'
+      ? Number(value)
+      : convertDMS(value[0], value[1])
+
   return (
     <div className='block-container'>
       <h1>{name}</h1>
       <div className='value'>
-        {recentValue && Number(recentValue).toFixed(2)}
+        {recentValue && recentValue}
         {unit && <span> {unit}</span>}
       </div>
       <div className='measurements'>
-        {measurements &&
-          measurements.map(([timestamp, value]) => {
-            let time = new Date(timestamp * 1000).toLocaleTimeString()
-            return <p key={timestamp}>{`${time} : ${value}`}</p>
-          })}
+        {measurements.length > 0
+          ? measurements.map(([timestamp, value]) => {
+              let time = new Date(timestamp * 1000).toLocaleTimeString()
+              return <p key={timestamp}>{`${time} : ${value}`}</p>
+            })
+          : 'No Data'}
       </div>
-      {/* <h4>{unit}</h4> */}
     </div>
   )
 }
